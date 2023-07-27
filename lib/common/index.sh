894cbdab615f3d803871b2d19e9c42ebe82d7138
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2023-07-27 20:10:39 +0700

    perf: update plugin from template [autocommit]

--- lib/common/index.sh
+++ lib/common/index.sh
@@ -17,10 +17,12 @@ export KC_ASDF_ORG KC_ASDF_NAME KC_ASDF_REPO
 ## Application information
 KC_ASDF_APP_TYPE="standalone"
 KC_ASDF_APP_NAME="cloudflared"
+KC_ASDF_APP_WEBS="https://developers.cloudflare.com/cloudflare-one/connections/connect-apps/install-and-setup/tunnel-guide"
 KC_ASDF_APP_DESC="A command-line client for Cloudflare Tunnel"
 KC_ASDF_APP_REPO="https://github.com/cloudflare/cloudflared"
 export KC_ASDF_APP_TYPE KC_ASDF_APP_NAME
-export KC_ASDF_APP_DESC KC_ASDF_APP_REPO
+export KC_ASDF_APP_WEBS KC_ASDF_APP_DESC
+export KC_ASDF_APP_REPO
 
 # shellcheck source-path=SCRIPTDIR/internal.sh
 source "${KC_ASDF_PLUGIN_PATH:?}/lib/common/internal.sh" || exit 1
